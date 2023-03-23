f876c7e4d7c9241dfc851f0a65ad43208ae51886
kimseyoung <rlatpdud0309@gmail.com>
2023-03-23 18:31:06 +0900

    업데이트

--- src/app/route.tsx
+++ src/app/route.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
-import { map } from "lodash";
 import routes from "./routes";
+import { map } from "lodash";
 
 const Route = () => {
   return (
