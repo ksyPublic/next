5bcdbededc58386b481de4d3fd4d53935ee372b4
kimseyoung <rlatpdud0309@gmail.com>
2023-03-27 16:27:43 +0900

    d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,11 @@
-// import axios from "axios";
-// import { CategoryResponse } from "../pages/api/utils/getCategoryList";
-
-// interface PageProps {
-//   categories: CategoryResponse[];
-// }
-
-// const Page = ({ categories }: PageProps) => {
-//   console.log("categories:", categories);
-//   // home
-//   return <main className="ly-main" />;
-// };
-
-// export async function getServerSideProps() {
-//   const host = process.env.HOST;
-//   const { data } = await axios.get(`${host}/api/category`);
-//   return {
-//     props: {
-//       categories: data,
-//     },
-//   };
-// }
-
-// export default Page;
 "use client";
 import { NextPage } from "next";
-import { useRouter } from "next/router";
+import { useRouter, useSearchParams } from "next/navigation";
 import axios from "axios";
 
 const AuthPage: NextPage = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const clientId = process.env.CAFE24_CLIENT_ID;
   const secretKey = process.env.CAFE24_CLIENT_SECRET;
 
@@ -40,7 +17,7 @@ const AuthPage: NextPage = () => {
   };
 
   const handleRedirect = async () => {
-    const { code } = router.query;
+    const code = searchParams.get("code");
     const redirectUri = `${window.location.origin}/auth`;
     const redirectUrl = `https://mall.cafe24api.com/api/v2/oauth/token?grant_type=authorization_code&code=${code}&client_id=${clientId}&client_secret=${secretKey}&redirect_uri=${redirectUri}`;
 
@@ -54,7 +31,7 @@ const AuthPage: NextPage = () => {
     // TODO: access_token과 refresh_token을 이용하여 CAFE24 API에 요청하는 코드 작성
   };
 
-  if (router.query.code) {
+  if (searchParams.get("code")) {
     handleRedirect();
   }
 
