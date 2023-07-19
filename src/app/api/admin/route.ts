492bd08a7ed435d222acf05b3f1bc6f9eb7817e6
ksyPublic <rlatpdud0309@gmail.com>
2023-07-19 17:48:04 +0900

    마지막이다

--- src/app/api/admin/route.ts
+++ src/app/api/admin/route.ts
@@ -4,18 +4,22 @@ import { customInitApp } from '@/store/admin'
 import validateSession from '@/utils/session';
 
 customInitApp();
-
 export async function GET(req: NextRequest, res: NextResponse) {
-    const userClaims = validateSession(req);
-    const snapshot = collection(db, 'admin')
-    try {
-        const menuData = (await getDocs(snapshot)).forEach((item) => {
-            return item.data()
-        })
-        return NextResponse.json(menuData, { status: 200 })
-    } catch (error) {
-        console.error("Error in GET /api/admin:", error);
-        return NextResponse.json({ message: 'Error fetching data' }, { status: 500 })
-    }
-}
+  const decodedClaims = await validateSession(req);
+  
+  // 유저의 클레임 정보에서 admin 클레임을 확인
+  const isAdmin = decodedClaims.admin;
 
+  if (isAdmin) {
+    const snapshot = collection(db, 'admin');
+    try {
+      const menuData = (await getDocs(snapshot)).forEach((item) => item.data());
+      return NextResponse.json(menuData, { status: 200 });
+    } catch (error) {
+      console.error("Error in GET /api/admin:", error);
+      return NextResponse.json({ message: 'Error fetching data' }, { status: 500 });
+    }
+  } else {
+    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  }
+}
--- src/app/utils/session.ts
+++ src/app/utils/session.ts
@@ -1,13 +1,11 @@
 // utils/session.ts
 import { NextRequest } from 'next/server'
-import admin, { auth } from 'firebase-admin'
-
-const uid = 'wleVzGN1zKM5RhmgWRTIr9V5Off2'
+import { auth } from 'firebase-admin'
 
 export default async function validateSession(request: NextRequest) {
+
   const session = request.cookies.get('session')?.value || ''
 
-  // Validate if the cookie exist in the request
   if (!session) {
     throw new Error('Session cookie does not exist.');
   }
@@ -15,19 +13,10 @@ export default async function validateSession(request: NextRequest) {
   // Use Firebase Admin to validate the session cookie
   const decodedClaims = await auth().verifySessionCookie(session, true)
 
-  const user = await admin.auth().getUser(uid)
-
-
-  if (user.customClaims && user.customClaims.admin === true) {
-    return
-  }
-
-  await admin.auth().setCustomUserClaims(uid, { admin: true })
-
-
   if (!decodedClaims) {
     throw new Error('Session cookie is invalid.');
   }
 
   return decodedClaims;
 }
+
