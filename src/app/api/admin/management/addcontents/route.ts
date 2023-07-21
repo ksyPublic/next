e90f0fcf19b42f8e44c67fc4daab99a162d24df4
ksyPublic <rlatpdud0309@gmail.com>
2023-07-21 18:41:00 +0900

    d

--- src/app/api/admin/management/addcontents/route.ts
+++ src/app/api/admin/management/addcontents/route.ts
@@ -1,6 +1,5 @@
 
-import { NextResponse } from 'next/server'
-import type { NextApiRequest } from 'next'
+import { NextResponse, NextRequest } from 'next/server'
 import { getDatabase, ref, child, push, update} from '@/store/database';
 
 type contentsDataProps = {
@@ -15,27 +14,27 @@ type contentsDataProps = {
   trailer ?:string
 }
 
-export async function POST(req: NextApiRequest, res: NextResponse) {
+export async function POST(req: NextRequest, res: NextResponse) {
   const db = getDatabase();
+  console.log('@@@', req.cookies.getAll())
+  // const {title, director, cast, genre, rating, release, summary, poster, trailer, key } = req.body;
 
-  const {title, director, cast, genre, rating, release, summary, poster, trailer, key } = req.body;
+  // const contentsData = {title, director, cast, genre, rating, release, summary, poster, trailer, key};
 
-  const contentsData = {title, director, cast, genre, rating, release, summary, poster, trailer, key};
+  // // Get a key for a new Post.
+  // const newPostRef = push(child(ref(db), key));
+  // const newPostKey = newPostRef.key;
 
-  // Get a key for a new Post.
-  const newPostRef = push(child(ref(db), key));
-  const newPostKey = newPostRef.key;
-
-  // If key generation was successful, update the database.
-  if (newPostKey) {
-  const updates: Record<string, contentsDataProps> = {};
-    updates['/contents/' + newPostKey] = contentsData;
+  // // If key generation was successful, update the database.
+  // if (newPostKey) {
+  // const updates: Record<string, contentsDataProps> = {};
+  //   updates['/contents/' + newPostKey] = contentsData;
     
-    // Perform the updates.
-    await update(ref(db), updates);
+  //   // Perform the updates.
+  //   await update(ref(db), updates);
     
-    NextResponse.json({ message: 'Success' }, {status:200});
-  } else {
-    NextResponse.json({ message: 'Error generating unique key' }, {status:500});
-  }
+  //   NextResponse.json({ message: 'Success' }, {status:200});
+  // } else {
+  //   NextResponse.json({ message: 'Error generating unique key' }, {status:500});
+  // }
 }
